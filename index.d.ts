d51693e6d90547d7be3f88e3f98e814060dcf668
Ivan Verevkin <vereva@x-root.org>
2016-05-23 23:27:43 +0700

    Typings add more detailed comment

--- index.d.ts
+++ index.d.ts
@@ -17,20 +17,73 @@ declare module "react-bootstrap-datetimepicker" {
 
 declare module ReactBootstrapDateTimePicker {
     interface DateTimeFieldProps extends React.Props<DateTimeField>{
-        // Optional
-        dateTime?: string;
-        format?: string;
-        inputFormat?: string;
-        onChange?: (value: string) => void;
-        showToday?: boolean;
-        size?: string;
-        daysOfWeekDisabled?: number[];
-        viewMode?: string | number;
-        inputProps?: React.HTMLProps<HTMLInputElement>;
-        minDate?: moment.Moment;
-        maxDate?: moment.Moment;
-        mode?: "date" | "time";
-        defaultText?: string;
+        /*
+          Represents the inital dateTime, this string is then parsed by moment.js
+          Default: moment().format('x')
+         */
+        dateTime?:string
+        /*
+          Defines the format moment.js should use to parse and output the date to onChange
+          Default: "x"
+         */
+        format?:string
+        /*
+          Defines the way the date is represented in the HTML input. It must be a format understanable by moment.js
+          Default: "MM/DD/YY h:mm A"
+         */
+        inputFormat?:string
+        /*
+          Callback trigger when the date changes.
+          x is the new datetime value in which represents Unix ms timestamp
+          Default: function	x => console.log(x)
+         */
+        onChange?:(x: string) => void
+        /*
+          Whether today's date should be highlighted
+          Default: true
+         */
+        showToday?:boolean
+        /*
+          Changes the size of the date picker input field. Sizes: "sm", "md", "lg"
+          Default: "md"
+         */
+        size?:string
+        /*
+          Disables clicking on some days. Goes from 0 (Sunday) to 6 (Saturday).
+          Default: []
+         */
+        daysOfWeekDisabled?:number[]
+        /*
+          The default view to display when the picker is shown. ('years', 'months', 'days')
+          Default: 'days'
+         */
+        viewMode?:string|number
+        /*
+          Defines additional attributes for the input element of the component.
+          Default: undefined
+         */
+        inputProps?:Object
+        /*
+          The earliest date allowed for entry in the calendar view.
+          Default: undefined
+         */
+        minDate?:moment.Moment
+        /*
+          The latest date allowed for entry in the calendar view.
+          Default: undefined
+         */
+        maxDate?:moment.Moment
+        /*
+          Allows to selectively display only the time picker ('time') or the date picker ('date')
+          Default: undefined
+         */
+        mode?:string
+        /*
+          Sets the initial value. Could be an empty string, or helper text.
+          Default: {dateTime}
+         */
+        defaultText?:string
+      }
     }
     interface DateTimeField extends React.ReactElement<DateTimeFieldProps> { }
 }
